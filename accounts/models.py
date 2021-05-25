df2867166fff64efffe9e25423ccbea4781671c0
Gagan Parmar <gaganparmar110@gmail.com>
2021-05-25 22:05:27 +0530

    add signup form

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+class UserRegisterDetails(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('D', 'Decline to answer'),
+    )
+    user_FirstName = models.CharField(max_length=220, null=True, blank=True)
+    user_LasttName = models.CharField(max_length=220, null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    dob = models.DateField(null=True, blank=True)
+    phone_number = models.IntegerField(null=True, blank=True)
+    email_id = models.EmailField(max_length=220, null=True, blank=True)
+    username = models.CharField(max_length=220, null=True, blank=True)
+    password1 = models.CharField(max_length=220, null=True, blank=True)
+    password2 = models.CharField(max_length=220, null=True, blank=True)
+
--- accounts/registerForm.py
+++ accounts/registerForm.py
@@ -1,42 +1,24 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django import forms
-from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('D', 'Decline to answer'),
+    )
+class SignUpForm(UserCreationForm):
+    user_FirstName = forms.CharField(max_length=100)
+    user_LasttName = forms.CharField(max_length=100)
+    phone_number = forms.IntegerField()
+    email_id = forms.EmailField(max_length=100)
+    username = forms.CharField(max_length=100)
+    dob = forms.DateField()
+    gender = forms.TypedChoiceField(choices=GENDER_CHOICES)
+
+    
 
 
-class CustomUserCreationForm(forms.Form):
-    username = forms.CharField(label='Enter Username', min_length=4, max_length=150)
-    # email = forms.EmailField(label='Enter email')
-    password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
-
-    def clean_username(self):
-        username = self.cleaned_data['username'].lower()
-        r = User.objects.filter(username=username)
-        if r.count():
-            raise  ValidationError("Username already exists")
-        return username
-
-    # def clean_email(self):
-    #     email = self.cleaned_data['email'].lower()
-    #     r = User.objects.filter(email=email)
-    #     if r.count():
-    #         raise  ValidationError("Email already exists")
-    #     return email
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Password don't match")
-
-        return password2
-
-    def save(self, commit=True):
-        user = User.objects.create_user(
-            self.cleaned_data['username'],
-            # self.cleaned_data['email'],
-            self.cleaned_data['password1']
-        )
-        return user
+    class Meta:
+        model = User
+        fields = ('username','user_FirstName','user_LasttName','phone_number', 'email_id','dob', 'password1', 'password2','gender' )
