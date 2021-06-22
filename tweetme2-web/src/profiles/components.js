c1a444dbbbf60c8c80e55f6d6ca5b5f1725b4058
ppratikcr7 <ppratik.cr7@gmail.com>
2021-06-23 02:55:57 +0530

    updated static files with https links

--- tweetme2-web/src/profiles/components.js
+++ tweetme2-web/src/profiles/components.js
@@ -35,7 +35,7 @@ export  function UserWhoToFollowDisplay(props){
 export  function UserPicture (props) {
   const {user, hideLink} = props
   const userIdSpan = <span className='mx-1 px-3 py-2 rounded-circle bg-dark text-white'>
-  {user.username[0]}
+  {"U"}
 </span>
   return  hideLink === true ? userIdSpan : <UserLink username={user.username}>{userIdSpan}</UserLink>
 }
