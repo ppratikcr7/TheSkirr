6af33192352a0c1e04e1ec09b7ff01ac413a7664
riddhi <daghariddhi12@gmail.com>
2021-06-28 16:36:08 +0530

    validations

--- accounts/registerForm.py
+++ accounts/registerForm.py
@@ -23,6 +23,9 @@ def validate_phoneNumber(value):
     if UserRegisterDetails.objects.filter(phone_number = value).exists():
         raise ValidationError((f"The contact: {value} is already taken. Please use another contact."),params = {'value':value})
 
+    if len(value) < 10:
+         raise ValidationError("Please add the correct phone number")
+         
 def validate_dob(value):
     try:
         datetime.datetime.strptime(str(value), '%Y-%m-%d')
@@ -39,32 +42,61 @@ def validate_gender(value):
 class DateInput(forms.DateInput):
     input_type = 'date'
 class SignUpForm(UserCreationForm):
+
     first_name = forms.CharField(max_length=100,widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'First Name', 'id': 'username'}))
+
+    first_name_public_access = forms.ChoiceField(widget=forms.RadioSelect(
+        attrs={'class': 'Radio', }), choices=(('True','Public'),('False','NonPublic'),))
+
     last_name = forms.CharField(max_length=100, required=False, widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'Last Name', 'id': 'username'}))
+
+    last_name_public_access = forms.ChoiceField(widget=forms.RadioSelect(
+        attrs={'class': 'Radio', }), choices=(('True','Public'),('False','NonPublic'),))
+
+    username = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'User Name', 'id': 'username'}))
+
     phone_regex = RegexValidator(regex=r'^\d{6,10}$', message="Phone number must be entered in the format: '9999999999'. Up to 10 digits allowed.")
-    phone_number = forms.CharField(validators=[phone_regex], max_length=10, widget=forms.TextInput(
+    phone_number = forms.CharField(validators=[phone_regex, validate_phoneNumber ], max_length=10, widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'Phone', 'id': 'username'})) # validators should be a list
+
+    phone_number_public_access = forms.ChoiceField(widget=forms.RadioSelect(
+        attrs={'class': 'Radio', }), choices=(('True','Public'),('False','NonPublic'),))
 
     # phone_number_public = forms.RadioSelect()
     email = forms.EmailField(validators = [validate_email], max_length=100, widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'Email ID', 'id': 'username'}))
+
+    email_public_access = forms.ChoiceField(widget=forms.RadioSelect(
+        attrs={'class': 'Radio', }), choices=(('True','Public'),('False','NonPublic'),))
+
     email2 = forms.EmailField(validators = [validate_email], required=False, max_length=100, widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'Secondary Email ID', 'id': 'username'}))
+
+    email2_public_access = forms.ChoiceField(widget=forms.RadioSelect(
+        attrs={'class': 'Radio', }), choices=(('True','Public'),('False','NonPublic'),))
     
     city = forms.CharField(max_length=100, widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'City', 'id': 'city'}))
-    username = forms.CharField(max_length=100, widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'User Name', 'id': 'username'}))
+    
     # dob = forms.DateField(widget=forms.TextInput(
     #     attrs={'class': 'datepicker', 'placeholder': 'DOB', 'id'Register: 'username'}))
     dob = forms.DateField(validators = [validate_dob, validate_future_dob], widget=DateInput(attrs={'class': 'datepicker', 'style': 'border-width: 1; border-color: #ced4da;', 'placeholder': 'YYYY-MM-DD', 'id': 'username'}))
     # dob = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
 
+    dob_public_access = forms.ChoiceField(widget=forms.RadioSelect(
+        attrs={'class': 'Radio', }), choices=(('True','Public'),('False','NonPublic'),))
+
     gender = forms.TypedChoiceField(validators = [validate_gender],choices=GENDER_CHOICES, widget=forms.Select(
         attrs={'class': 'form-control', 'placeholder': 'Select Gender', 'id': 'username'}))
+    
+    gender_public_access = forms.ChoiceField(widget=forms.RadioSelect(
+        attrs={'class': 'Radio', }), choices=(('True','Public'),('False','NonPublic'),))
+    
     areaOfInterest = forms.CharField(required=False, widget=forms.Textarea(attrs={'cols':50, 'rows': 3 , 'style': 'border-width: 1; border-color: #ced4da;', 'placeholder':' Write your area of interest here!'}))
+    
     password1 = forms.CharField(widget=forms.PasswordInput(
             attrs={
                 'class': 'form-control',
